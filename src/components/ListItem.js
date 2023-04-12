646b6f26bf895b006180768d4fa5c909c8ff3fcc
JWJung-99 <39busy@naver.com>
2023-04-12 23:47:07 +0900

    rn-todo_final

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,7 +4,7 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { DANGER, BLACK, PRIMARY, GRAY } from '../Colors';
 
-const ListItem = memo(({ item }) => {
+const ListItem = memo(({ item, onDelete, onToggle }) => {
   const checkboxProps = {
     name: item.isDone ? 'checkbox-marked' : 'checkbox-blank-outline',
     color: item.isDone ? PRIMARY.DEFAULT : BLACK,
@@ -13,7 +13,7 @@ const ListItem = memo(({ item }) => {
 
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => {}} hitSlop={10}>
+      <Pressable onPress={() => onToggle(item.id)} hitSlop={10}>
         <MaterialCommunityIcons {...checkboxProps} />
       </Pressable>
 
@@ -21,7 +21,7 @@ const ListItem = memo(({ item }) => {
         <Text style={item.isDone && { color: GRAY.DEFAULT }}>{item.task}</Text>
       </View>
 
-      <Pressable onPress={() => {}} hitSlop={10}>
+      <Pressable onPress={() => onDelete(item.id)} hitSlop={10}>
         <MaterialCommunityIcons
           name="trash-can"
           size={20}
@@ -36,6 +36,7 @@ ListItem.displayName = 'ListItem';
 
 ListItem.propTypes = {
   item: PropTypes.object.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 const styles = StyleSheet.create({
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,17 +1,89 @@
-import { View } from 'react-native';
+import 'react-native-get-random-values';
+import { Alert, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import EmptyList from '../components/EmptyList';
 import List from '../components/List';
 import InputFAB from '../components/InputFAB';
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 const ListScreen = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, task: 'task 1', isDone: false },
+    { id: 2, task: 'task 2', isDone: false },
+    { id: 3, task: 'task 3', isDone: false },
+    { id: 4, task: 'task 4', isDone: false },
+    { id: 5, task: 'task 5', isDone: false },
+    { id: 6, task: 'task 6', isDone: false },
+    { id: 7, task: 'task 7', isDone: false },
+    { id: 8, task: 'task 8', isDone: false },
+    { id: 9, task: 'task 9', isDone: false },
+    { id: 10, task: 'task 10', isDone: false },
+    { id: 11, task: 'task 11', isDone: false },
+    { id: 12, task: 'task 12', isDone: false },
+  ]);
   const { bottom } = useSafeAreaInsets();
-  const todos = [];
+
+  const [isBottom, setIsBottom] = useState(false);
+
+  const { getItem, setItem } = useAsyncStorage('todos');
+
+  const save = async (data) => {
+    try {
+      await setItem(JSON.stringify(data));
+      setTodos(data);
+    } catch (e) {
+      Alert.alert('저장하기 실패', '데이터 저장하기에 실패했습니다.');
+    }
+  };
+
+  const load = async () => {
+    try {
+      const data = await getItem();
+      const todos = JSON.parse(data || '[]');
+      setTodos(todos);
+    } catch (e) {
+      Alert.alert('불러오기 실패', '데이터 불러오기에 실패했습니다.');
+    }
+  };
+
+  useEffect(() => {
+    load();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onInsert = (task) => {
+    const id = nanoid();
+    const newTodos = [{ id, task, isDone: false }, ...todos];
+    save(newTodos);
+  };
+
+  const onDelete = (id) => {
+    const newTodos = todos.filter((item) => item.id !== id);
+    save(newTodos);
+  };
+
+  const onToggle = (id) => {
+    const newTodos = todos.map((item) =>
+      item.id === id ? { ...item, isDone: !item.isDone } : item
+    );
+    save(newTodos);
+  };
 
   return (
     <View style={{ flex: 1, paddingBottom: bottom }}>
-      {todos.length ? <List data={todos} /> : <EmptyList />}
-      <InputFAB />
+      {todos.length ? (
+        <List
+          data={todos}
+          setIsBottom={setIsBottom}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
+      ) : (
+        <EmptyList />
+      )}
+      <InputFAB onInsert={onInsert} isBottom={isBottom} />
     </View>
   );
 };
